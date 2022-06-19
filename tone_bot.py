63d6240eb5451079d0dbed586854245634f1db91
gon <gon.ven.nunes@gmail.com>
2022-06-19 17:38:41 +0100

    complete

--- tone_bot.py
+++ tone_bot.py
@@ -1,10 +1,14 @@
-import interactions
+import discord
 from info import tones, token
 
 tones, token = tones(), token()
 
-bot = interactions.Client(token=token)
+bot = discord.Client()
 
+@bot.event
+async def on_message(message: discord.Message):
+    for tone in tones.keys():
+        if tone in message.content.split(' '):
+            await message.channel.send(f'{message.author.mention}: ``{tones[tone]}``')
 
-
-bot.start()
+bot.run(token)
