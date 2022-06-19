cd206e0fcac488a9320a002f5ed1ad0e8813674e
gon <gon.ven.nunes@gmail.com>
2022-06-19 17:43:53 +0100

    better comments

--- tone_bot.py
+++ tone_bot.py
@@ -1,14 +1,20 @@
 import discord
 from info import tones, token
 
+# get the tones dict and the token str
 tones, token = tones(), token()
 
+# start the bot
 bot = discord.Client()
 
+# event reader
 @bot.event
 async def on_message(message: discord.Message):
     for tone in tones.keys():
         if tone in message.content.split(' '):
+            
+            # @ the user and send the meaning
             await message.channel.send(f'{message.author.mention}: ``{tones[tone]}``')
 
+# loop
 bot.run(token)
