8d6533f036ebd2ecbfef6adc8d99670fb142c9ee
gon <gon.ven.nunes@gmail.com>
2022-06-19 18:27:35 +0100

    changed comments

--- info.py
+++ info.py
@@ -3,24 +3,19 @@ import bs4
 
 # function to get the dictionary properly edited
 def gettones():
-    
-    # get the table from https://toneindicators.carrd.co/#masterlist
     table = bs4.BeautifulSoup(get('https://toneindicators.carrd.co/#masterlist').text, 'html.parser').find_all('td')
 
-    # generate a dictionary with the tones and their meaning
     inds = dict()
     for i in range(0, len(table)-1, 2):
         item = table[i].get_text()
         item1 = table[i+1].get_text()
         
-        # check if multiple possible indicators
         if len(item.split(' or ')) > 1:
             for poss in item.split(' or '):
                 inds[poss] = item1
         else:
             inds[item] = item1
             
-    # make /nbh's meaning more compact
     inds['/nbh'] = '@ nobody here'
 
     return inds
