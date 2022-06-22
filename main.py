965f41fbfc6d6876fa777549cd003ec8921ea9d1
gon <gon.ven.nunes@gmail.com>
2022-06-22 01:27:34 +0100

    changed what and mean to tone and ind

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ async def on_message(message: discord.Message):
             if message.author.dm_channel is None:
                 await message.author.create_dm()
             await message.author.dm_channel.send(question)
-        case 't?what':
+        case 't?tone':
             await message.channel.send(whattone(message.content, tones))
-        case 't?mean':
+        case 't?ind':
             await message.channel.send(meanind(message.content, tones))
         case _:
             line = toneused(message.content, tones)
