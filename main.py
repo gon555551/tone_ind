a464f791c8479ab0099e411280ff3496047526d4
gon <gon.ven.nunes@gmail.com>
2022-06-26 17:44:59 +0100

    __name__

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 import discord, os, dotenv
 from func import *
 
-# start the bot
-bot = discord.Client()
 
-# get the handler, load .env
-handler = EventHandler(get_tones())
-dotenv.load_dotenv()
+if __name__ == '__main__':
+    # start the bot
+    bot = discord.Client()
 
-# on ready
-@bot.event
-async def on_ready():
-    print(f"<{bot.user}> is ready.")
-    return
+    # get the handler, load .env
+    handler = EventHandler(get_tones())
+    dotenv.load_dotenv()
 
-
-# on message
-@bot.event
-async def on_message(message: discord.Message):
-    if message.author == bot.user:
+    # on ready
+    @bot.event
+    async def on_ready():
+        print(f"<{bot.user}> is ready.")
         return
 
-    match message.content.split(" ")[0]:
-        case "t?all":
-            if message.author.dm_channel is None:
-                await message.author.create_dm()
-            await message.author.dm_channel.send(handler.question)
-        case "t?tone":
-            await message.channel.send(handler.what_tone(message.content))
-        case "t?ind":
-            await message.channel.send(handler.mean_ind(message.content))
-        case _:
-            line = handler.tone_used(message.content)
-            if line != "":
-                await message.reply(line, mention_author=False)
+
+    # on message
+    @bot.event
+    async def on_message(message: discord.Message):
+        if message.author == bot.user:
+            return
+
+        match message.content.split(" ")[0]:
+            case "t?all":
+                if message.author.dm_channel is None:
+                    await message.author.create_dm()
+                await message.author.dm_channel.send(handler.question)
+            case "t?tone":
+                await message.channel.send(handler.what_tone(message.content))
+            case "t?ind":
+                await message.channel.send(handler.mean_ind(message.content))
+            case _:
+                line = handler.tone_used(message.content)
+                if line != "":
+                    await message.reply(line, mention_author=False)
 
 
-# loop
-bot.run(os.environ["TOKEN"])
+    # loop
+    bot.run(os.environ["TOKEN"])
